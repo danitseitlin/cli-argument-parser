a5fd75ad5becf4b5a0c0818fd9a106de51c4e8bc
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-06-27 23:58:21 +0300

    ongoing development

--- index.ts
+++ index.ts
@@ -1,25 +1,28 @@
 import * as file from 'file-exists';
 
 /**
- * A global variable containing a parsed list of loaded CLI arguments by preferred prefix (--) and equality sign (=)
+ * A global variable containing a parsed list of loaded CLI arguments by preferred prefix (--) and seperator (=)
  */
 export const cliArguments = loadArguments();
 
 /**
- * Filtering only relevant arguments by preferred prefix (--) and equality sign (=)
+ * Loading the filtered arguments
  */
 function loadArguments(): {[key: string]: string} {
-    let config: config = file.sync('./cli.config.json') ? require('./cli.config.json'): {prefix: '--', seperator: '='}
-    console.log(config)
+    let config: config = file.sync('./cli.config.json') ? require('./cli.config.json'): { prefix: '--', seperator: '=' }
     config = Object.assign(config, filterArguments('--cli-', '='))
-    console.log(config)
     return filterArguments(config.prefix, config.seperator);
 }
 
-function filterArguments(prefix: string, seperator: string) {
+/**
+ * Filtering only relevant arguments by preferred prefix (--) and seperator (=)
+ * @param prefix The prefix of the argument
+ * @param seperator The seperator of the argument
+ */
+function filterArguments(prefix: string, seperator: string): {[key: string]: string} {
     const json = {};
-    const cliArguments = process.argv.filter((argument: string) => argument.startsWith(prefix) && argument.indexOf(seperator) !== -1)
-    for(const argument of cliArguments) {
+    const argumentsList = process.argv.filter((argument: string) => argument.startsWith(prefix) && argument.indexOf(seperator) !== -1)
+    for(const argument of argumentsList) {
         const splitArgument: string[] = argument.split(seperator);
         const name = splitArgument[0].replace(prefix, '')
         const value = splitArgument[1]
