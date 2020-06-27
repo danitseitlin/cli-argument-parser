b0df6fb431dfc8074e953ecb935fd37d2edf46ec
Dani Tseiltin <39850374+danitseitlin@users.noreply.github.com>
2020-06-28 00:47:22 +0300

    Adding CLI arguments for the configurations (#2)
    * 0.0.6
    
    * ongoing development
    
    * ongoing development
    
    * update package.json
    
    * Code review changes

--- index.ts
+++ index.ts
@@ -1,33 +1,42 @@
 import * as file from 'file-exists';
 
 /**
- * A global variable containing a parsed list of loaded CLI arguments by preferred prefix (--) and equality sign (=)
+ * A global variable containing a parsed list of loaded CLI arguments by preferred prefix (--) and seperator (=)
  */
 export const cliArguments = loadArguments();
 
 /**
- * Filtering only relevant arguments by preferred prefix (--) and equality sign (=)
+ * Loading the filtered arguments
  */
 function loadArguments(): {[key: string]: string} {
-    const loadedArguments: {[key: string]: string} = {};
-    const config: config = file.sync('./cli.config.json') ? require('./cli.config.json') : {prefix: '--', equalitySign: '='}
-    for(const argument of process.argv) {
-        if(argument.indexOf(config.equalitySign) !== -1 && argument.startsWith(config.prefix)) {
-            const splitArgument: string[] = argument.split(config.equalitySign);
-            const name = splitArgument[0].replace(config.prefix, '');
-            const value = splitArgument[1];
-            loadedArguments[name] = value;
-        }
+    let config: config = file.sync('./cli.config.json') ? require('./cli.config.json'): { prefix: '--', seperator: '=' }
+    config = Object.assign(config, filterArguments('--cli-', '='))
+    return filterArguments(config.prefix, config.seperator);
+}
+
+/**
+ * Filtering only relevant arguments by preferred prefix (--) and seperator (=)
+ * @param prefix The prefix of the argument
+ * @param seperator The seperator of the argument
+ */
+function filterArguments(prefix: string, seperator: string): {[key: string]: string} {
+    const finalArgumentsList = {};
+    const argumentsList = process.argv.filter((argument: string) => argument.startsWith(prefix) && argument.indexOf(seperator) !== -1)
+    for(const argument of argumentsList) {
+        const splitArgument = argument.split(seperator);
+        const name = splitArgument[0].replace(prefix, '')
+        const value = splitArgument[1]
+        finalArgumentsList[name] = value;
     }
-    return loadedArguments;
+    return finalArgumentsList;
 }
 
 /**
  * The config structure
  * @param prefix A starting prefix for the arguments (default: --)
- * @param equalitySign An equality sign for the arguments (default: =)
+ * @param seperator A seperator sign for the arguments (default: =)
  */
 type config = {
     prefix: string,
-    equalitySign: string
+    seperator: string
 }
