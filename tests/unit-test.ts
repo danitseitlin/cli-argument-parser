571b51194417ce34384e549db6e0b08cba4b1b0f
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-07-01 01:38:02 +0300

    Adding improvement

--- tests/unit-test.ts
+++ tests/unit-test.ts
@@ -1,4 +1,4 @@
-import { cliArguments } from '../index';
+import { cliArguments, filterArguments } from '../index';
 import { expect } from 'chai'
 describe('Sanity testing', async function() {
     it('Positive: Expected arguments', async () => {
@@ -7,5 +7,18 @@ describe('Sanity testing', async function() {
     it('Negative: Expected arguments', async () => {
         expect(cliArguments.y).to.equal(undefined, 'The value of y')
     });
+    it('filterArguments function', async () => {
+        process.argv = ['x', '--y', 'z=1'];
+        let filteredArguments = filterArguments('', '');
+        expect(filteredArguments.x).to.equal('', 'The value of x')
+        expect(filteredArguments.y).to.equal(undefined, 'The value of y')
+        filteredArguments = filterArguments('--', '');
+        expect(filteredArguments.x).to.equal(undefined, 'The value of x')
+        expect(filteredArguments.y).to.equal('', 'The value of y')
+        filteredArguments = filterArguments('', '=');
+        expect(filteredArguments.x).to.equal(undefined, 'The value of x')
+        expect(filteredArguments.y).to.equal(undefined, 'The value of y')
+        expect(filteredArguments.z).to.equal('1', 'The value of z')
+    })
 });
 
