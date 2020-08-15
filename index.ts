daa9bfcb2b238c9ee1dc86d3466ff78a03e74a94
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-08-15 23:01:32 +0300

    Renaming seperator to separator

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as file from 'file-exists';
 
 /**
- * A global variable containing a parsed list of loaded CLI arguments by preferred prefix (--) and seperator (=)
+ * A global variable containing a parsed list of loaded CLI arguments by preferred prefix (--) and separator (=)
  */
 export const cliArguments = loadArguments();
 
@@ -9,21 +9,21 @@ export const cliArguments = loadArguments();
  * Loading the filtered arguments
  */
 function loadArguments(): {[key: string]: string} {
-    let config: config = file.sync('./cli.config.json') ? require(`${process.env.INIT_CWD}/cli.config.json`): { prefix: '--', seperator: '=' }
+    let config: config = file.sync('./cli.config.json') ? require(`${process.env.INIT_CWD}/cli.config.json`): { prefix: '--', separator: '=' }
     config = Object.assign(config, filterArguments('--cli-', '='))
-    return filterArguments(config.prefix, config.seperator);
+    return filterArguments(config.prefix, config.separator);
 }
 
 /**
- * Filtering only relevant arguments by preferred prefix (--) and seperator (=)
+ * Filtering only relevant arguments by preferred prefix (--) and separator (=)
  * @param prefix The prefix of the argument
- * @param seperator The seperator of the argument
+ * @param separator The separator of the argument
  */
-export function filterArguments(prefix: string, seperator: string): {[key: string]: string} {
+export function filterArguments(prefix: string, separator: string): {[key: string]: string} {
     const finalArgumentsList = {};
-    const argumentsList = filterer(prefix, seperator);
+    const argumentsList = filterer(prefix, separator);
     for(const argument of argumentsList) {
-        const splitArgument = (seperator !== '') ? argument.split(seperator): [argument, undefined];
+        const splitArgument = (separator !== '') ? argument.split(separator): [argument, undefined];
         const name = splitArgument[0].replace(prefix, '')
         const value = (splitArgument[1] !== undefined) ? splitArgument[1]: ''
         finalArgumentsList[name] = value;
@@ -32,26 +32,26 @@ export function filterArguments(prefix: string, seperator: string): {[key: strin
 }
 
 /**
- * A filter function to filter by dynamic prefix & seperator
+ * A filter function to filter by dynamic prefix & separator
  * @param prefix The prefix of the argument
- * @param seperator The seperator of the argument 
+ * @param separator The separator of the argument 
  */
-function filterer(prefix: string, seperator: string): string[] {
-    if(prefix !== '' && seperator === '')  
+function filterer(prefix: string, separator: string): string[] {
+    if(prefix !== '' && separator === '')  
         return process.argv.filter((argument: string) => argument.startsWith(prefix))
-    else if(prefix === '' && seperator !== '')
-        return process.argv.filter((argument: string) => argument.indexOf(seperator) !== -1)
-    else if(prefix !== '' && seperator !== '')
-        return process.argv.filter((argument: string) => argument.startsWith(prefix) && argument.indexOf(seperator) !== -1)
+    else if(prefix === '' && separator !== '')
+        return process.argv.filter((argument: string) => argument.indexOf(separator) !== -1)
+    else if(prefix !== '' && separator !== '')
+        return process.argv.filter((argument: string) => argument.startsWith(prefix) && argument.indexOf(separator) !== -1)
     return process.argv;
 }
 
 /**
  * The config structure
  * @param prefix A starting prefix for the arguments (default: --)
- * @param seperator A seperator sign for the arguments (default: =)
+ * @param separator A separator sign for the arguments (default: =)
  */
 type config = {
     prefix: string,
-    seperator: string
+    separator: string
 }
